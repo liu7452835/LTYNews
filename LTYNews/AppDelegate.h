e6133b1408b5dd2861f835cf9e8bdd35947e8b35
刘天宇 <lty@liutianyudemacbook-pro.local>
2018-11-12 21:08:29 +0800

    Initial Commit

---
+++ LTYNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LTYNews
+//
+//  Created by 刘天宇 on 2018/11/12.
+//  Copyright © 2018 lty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LTYNews/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LTYNews
+//
+//  Created by 刘天宇 on 2018/11/12.
+//  Copyright © 2018 lty. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
